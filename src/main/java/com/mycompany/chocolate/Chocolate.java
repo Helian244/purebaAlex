bf53000c41592b832aa97596017adf2d8d4b04db
Alex Paca <fujorita772@gmail.com>
2023-10-15 22:10:44 -0400

    no romper

--- src/main/java/com/mycompany/chocolate/Chocolate.java
+++ src/main/java/com/mycompany/chocolate/Chocolate.java
@@ -12,10 +12,8 @@ public class Chocolate {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< HEAD
         System.out.println("chanchos malos!");
-=======
         System.out.println("HOLA ANDRELO");
->>>>>>> 8589c9384e469cda157b9935e4a1bbd87a0df8ee
+        System.out.println("NO lo rompas manito");
     }
 }
